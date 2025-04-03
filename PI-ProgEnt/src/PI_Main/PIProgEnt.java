0d73c2694f35aec0a90d63017612c7f39f2761fe
NereidaRO <nereidarodriguezorenes@gmail.com>
2025-04-03 20:29:52 +0200

    added basic package structure

--- PI-ProgEnt/src/PI_Main/PIProgEnt.java
+++ PI-ProgEnt/src/PI_Main/PIProgEnt.java
@@ -2,11 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package pi.progent;
+package PI_Main;
 
 /**
- *
- * @author Asus
+ * @since 03/04/2025
+ * @author Diego Bermejo Álvarez, Ayman El Hattachi, Nereida Rodríguez Orenes - 1ºDAM
+ * Proyecto Intermodular - Entornos de Desarrollo y Programación
  */
 public class PIProgEnt {
 
@@ -14,7 +15,7 @@ public class PIProgEnt {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Aquí van los menús (para pasar a las distintas gestiones y esas cosas)
     }
     
 }
---
+++ PI-ProgEnt/src/PI_Tablas/Repara.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PI_Tablas;
+
+/**
+ * @since 03/04/2025
+ * @author Diego Bermejo Álvarez, Ayman El Hattachi, Nereida Rodríguez Orenes - 1ºDAM
+ * Proyecto Intermodular - Entornos de Desarrollo y Programación
+ */
+public class Repara {
+    
+}
