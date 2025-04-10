d83f88c7e11c947a857785ffab107aa9c5568516
NereidaRO <nereidarodriguezorenes@gmail.com>
2025-04-10 15:41:37 +0200

    Scanner corregido

--- PI-ProgEnt/src/PI_Main/PIProgEnt.java
+++ PI-ProgEnt/src/PI_Main/PIProgEnt.java
@@ -26,13 +26,6 @@ public class PIProgEnt {
         Scanner sc = new Scanner(System.in);
         int opt = 0;
         Gestion gest = new Gestion();
-        //gest.insertarAveria(Date.valueOf("2025-03-08"), Date.valueOf("2025-04-08"), 1, 3000, "Cuchilla rota", "Buscar otro proveedor", 3);
-        //gest.insertarMaquina("Maquina QRT", "fresadora", "modulo 3", 2500, true, Date.valueOf("2025-06-08"), 5);
-        //gest.insertarTarea("Reparar tabletas", "Reparar el soporte de esta aplicacion");
-        //gest.eliminarTarea(27);
-        //gest.modificarAveria(3, Date.valueOf("2025-03-08"), Date.valueOf("2025-04-08"), 2, 500, "Torno derretido", "Sin comentarios", 5);
-        //gest.modificarMaquina(6, "Maquina SSD", "Apiladora", "modulo 15", 150, true, Date.valueOf("2025-07-08"), 7);
-        //gest.modificarTarea(4, "Afilar", "Afilar las cuchillas de las máquinas de corte");
         // Menú inicial:
         System.out.println("Hola, ¿qué tipo de gestión quiere hacer? \n"
                 + "1 - Gestionar usuarios \n"
@@ -42,6 +35,7 @@ public class PIProgEnt {
                 + "0 - Salir \n"
         );
         opt = sc.nextInt();
+        sc.nextLine();
         while (opt!=0){
             switch(opt){
                 case 1:
@@ -67,19 +61,19 @@ public class PIProgEnt {
                             java.sql.Date fechaNacim;
                             
                             System.out.println("Escriba el NIF: ");
-                            NIF = sc.next();
+                            NIF = sc.nextLine();
                             System.out.println("Escriba el nombre (JoseAntonio): ");
-                            nombre = sc.next();
+                            nombre = sc.nextLine();
                             System.out.println("Escriba los apellidos (GarciaGomez): ");
-                            apellidos = sc.next();
+                            apellidos = sc.nextLine();
                             System.out.println("Escriba la dirección: ");
-                            direccion = sc.next();
+                            direccion = sc.nextLine();
                             System.out.println("Escriba el email: ");
-                            email = sc.next();
+                            email = sc.nextLine();
                             System.out.println("Escriba el rol (admin, operario, mecanico): ");
-                            rol = sc.next();
+                            rol = sc.nextLine();
                             System.out.println("Escriba la fecha de nacimiento (aaaa-mm-dd): ");
-                            fechaNacim = java.sql.Date.valueOf(sc.next());  
+                            fechaNacim = java.sql.Date.valueOf(sc.nextLine());  
                             
                             gest.insertarUsuario(NIF, nombre, apellidos, direccion, email, rol, fechaNacim);
                             
@@ -90,20 +84,21 @@ public class PIProgEnt {
                             //El resto de las variables están en case 1; Java entiende que son para todos los casos
                             System.out.println("Escriba el ID del usuario en la base de datos: ");
                             userID = sc.nextInt();
+                            sc.nextLine();
                             System.out.println("Escriba el NIF: ");
-                            NIF = sc.next();
+                            NIF = sc.nextLine();
                             System.out.println("Escriba el nombre (JoseAntonio): ");
-                            nombre = sc.next();
+                            nombre = sc.nextLine();
                             System.out.println("Escriba los apellidos (GarciaGomez): ");
-                            apellidos = sc.next();
+                            apellidos = sc.nextLine();
                             System.out.println("Escriba la dirección: ");
-                            direccion = sc.next();
+                            direccion = sc.nextLine();
                             System.out.println("Escriba el email: ");
-                            email = sc.next();
+                            email = sc.nextLine();
                             System.out.println("Escriba el rol (admin, operario, mecanico): ");
-                            rol = sc.next();
+                            rol = sc.nextLine();
                             System.out.println("Escriba la fecha de nacimiento (aaaa-mm-dd): ");
-                            fechaNacim = java.sql.Date.valueOf(sc.next()); 
+                            fechaNacim = java.sql.Date.valueOf(sc.nextLine()); 
                             
                             gest.modificarUsuario(userID, NIF, nombre, apellidos, direccion, email, rol, fechaNacim);
                             
@@ -112,6 +107,7 @@ public class PIProgEnt {
                         case 3:
                             System.out.println("Escriba el ID del usuario que busca: ");
                             int idUsuario = sc.nextInt();
+                            sc.nextLine();
                             gest.buscarUsuario(idUsuario);
                             opt=0;
                             break;
@@ -122,6 +118,7 @@ public class PIProgEnt {
                         case 5:
                             System.out.println("Escriba el ID del usuario que quiere eliminar: ");
                             userID = sc.nextInt();
+                            sc.nextLine();
                             gest.eliminarUsuario(userID);
                             opt=0;
                             break;
@@ -150,21 +147,24 @@ public class PIProgEnt {
                             java.sql.Date fechaManten;
                             
                             System.out.println("Escriba una descripción breve: ");
-                            maquinaDescrip = sc.next();
+                            maquinaDescrip = sc.nextLine();
                             System.out.println("Escriba el tipo de máquina: ");
-                            tipo = sc.next();
+                            tipo = sc.nextLine();
                             System.out.println("Escriba la ubicación: ");
-                            ubicacion = sc.next();
+                            ubicacion = sc.nextLine();
                             System.out.println("Escriba cuántas horas de uso tiene: ");
                             horas = sc.nextInt();
+                            sc.nextLine();
                             //Por defecto, las máquinas nuevas están activas (no deberían estar rotas si son nuevas)
                             status = true;
                             System.out.println("Escriba la fecha del próximo mantenimiento (aaaa-mm-dd): ");
-                            fechaManten = java.sql.Date.valueOf(sc.next()); 
+                            fechaManten = java.sql.Date.valueOf(sc.nextLine()); 
                             System.out.println("Escriba el ID del responsable: ");
                             responsableID = sc.nextInt();
+                            sc.nextLine();
                             
                             gest.insertarMaquina(maquinaDescrip, tipo, ubicacion, horas, status, fechaManten, responsableID);
+                            
                             opt=0;
                             break;
                         case 2:
@@ -173,25 +173,28 @@ public class PIProgEnt {
                             
                             System.out.println("Escriba el ID de la máquina que quiere modificar: ");
                             maqID = sc.nextInt();
+                            sc.nextLine();
                             System.out.println("Escriba una descripción breve: ");
-                            maquinaDescrip = sc.next();
+                            maquinaDescrip = sc.nextLine();
                             System.out.println("Escriba el tipo de máquina: ");
-                            tipo = sc.next();
+                            tipo = sc.nextLine();
                             System.out.println("Escriba la ubicación: ");
-                            ubicacion = sc.next();
+                            ubicacion = sc.nextLine();
                             System.out.println("Escriba cuántas horas de uso tiene: ");
                             horas = sc.nextInt();
+                            sc.nextLine();
                             System.out.println("Escriba 'r' si la máquina está rota y otra letra si está activa");
-                            aux = sc.next();
+                            aux = sc.nextLine();
                             if (aux == "r"){
                                 status = false;
                             }else{
                                 status = true;
                             }
                             System.out.println("Escriba la fecha del próximo mantenimiento (aaaa-mm-dd): ");
-                            fechaManten = java.sql.Date.valueOf(sc.next()); 
+                            fechaManten = java.sql.Date.valueOf(sc.nextLine()); 
                             System.out.println("Escriba el ID del responsable: ");
                             responsableID = sc.nextInt();
+                            sc.nextLine();
                             
                             gest.modificarMaquina(maqID, maquinaDescrip, tipo, ubicacion, horas, status, fechaManten, responsableID);
                             
@@ -200,6 +203,7 @@ public class PIProgEnt {
                         case 3:
                             System.out.println("Escriba el ID de la máquina que busca: ");
                             int idMaquina = sc.nextInt();
+                            sc.nextLine();
                             gest.buscarUsuario(idMaquina);
                             opt=0;
                             break;
@@ -210,6 +214,7 @@ public class PIProgEnt {
                         case 5:
                             System.out.println("Escriba el ID de la máquina que quiere eliminar: ");
                             maqID = sc.nextInt();
+                            sc.nextLine();
                             gest.eliminarMaquina(maqID);
                             opt=0;
                             break;
@@ -237,20 +242,23 @@ public class PIProgEnt {
                             int usuarioID;
                             
                             System.out.println("Escriba la fecha de reporte (aaaa-mm-dd): ");
-                            fechaReporte = java.sql.Date.valueOf(sc.next()); 
+                            fechaReporte = java.sql.Date.valueOf(sc.nextLine()); 
                             System.out.println("Escriba la fecha de reparacion (aaaa-mm-dd): ");
-                            fechaReparacion = java.sql.Date.valueOf(sc.next()); 
+                            fechaReparacion = java.sql.Date.valueOf(sc.nextLine()); 
                             System.out.println("Escriba cuántas horas estuvo parada la máquina: ");
                             duracion = sc.nextInt();
+                            sc.nextLine();
                             System.out.println("Escriba el coste de la avería: ");
                             coste = sc.nextInt(); 
+                            sc.nextLine();
                             System.out.println("Escriba una descripción breve: ");
-                            averiaDescrip = sc.next();
+                            averiaDescrip = sc.nextLine();
                             System.out.println("Escriba un comentario: ");
-                            comentario = sc.next();
+                            comentario = sc.nextLine();
                             //Aunque esto sea redundante, no poner el reportador da conflictos con la base de datos
                             System.out.println("Escriba quien reportó la avería: ");
                             usuarioID = sc.nextInt();
+                            sc.nextLine();
                             
                             gest.insertarAveria(fechaReporte, fechaReparacion, duracion, coste, averiaDescrip, comentario, usuarioID);
                             
@@ -261,21 +269,25 @@ public class PIProgEnt {
                             int avID;
                             System.out.println("Escriba el ID de la avería que quiere modificar: ");
                             avID = sc.nextInt();
+                            sc.nextLine();
                             System.out.println("Escriba la fecha de reporte (aaaa-mm-dd): ");
-                            fechaReporte = java.sql.Date.valueOf(sc.next()); 
+                            fechaReporte = java.sql.Date.valueOf(sc.nextLine()); 
                             System.out.println("Escriba la fecha de reparacion (aaaa-mm-dd): ");
-                            fechaReparacion = java.sql.Date.valueOf(sc.next()); 
+                            fechaReparacion = java.sql.Date.valueOf(sc.nextLine()); 
                             System.out.println("Escriba cuántas horas estuvo parada la máquina: ");
                             duracion = sc.nextInt();
+                            sc.nextLine();
                             System.out.println("Escriba el coste de la avería: ");
                             coste = sc.nextInt(); 
+                            sc.nextLine();
                             System.out.println("Escriba una descripción breve: ");
-                            averiaDescrip = sc.next();
+                            averiaDescrip = sc.nextLine();
                             System.out.println("Escriba un comentario: ");
-                            comentario = sc.next();
+                            comentario = sc.nextLine();
                             //Aunque esto sea redundante, no poner el reportador da conflictos con la base de datos
                             System.out.println("Escriba quien reportó la avería: ");
                             usuarioID = sc.nextInt();
+                            sc.nextLine();
                             
                             gest.modificarAveria(avID, fechaReporte, fechaReparacion, duracion, coste, averiaDescrip, comentario, usuarioID);
                             
@@ -285,6 +297,7 @@ public class PIProgEnt {
                             //code
                             System.out.println("Escriba el ID de la avería que busca: ");
                             avID = sc.nextInt();
+                            sc.nextLine();
                             gest.buscarAveria(avID);
                             opt=0;
                             break;
@@ -295,6 +308,7 @@ public class PIProgEnt {
                         case 5:
                             System.out.println("Escriba el ID de la avería que quiere eliminar: ");
                             avID = sc.nextInt();
+                            sc.nextLine();
                             gest.eliminarAveria(avID);
                             opt=0;
                             break;
@@ -318,9 +332,9 @@ public class PIProgEnt {
                             String tareaDescrip;
 
                             System.out.println("Escriba el nombre de la tarea: ");
-                            tareaNombre = sc.next();
+                            tareaNombre = sc.nextLine();
                             System.out.println("Escriba una descripción breve: ");
-                            tareaDescrip = sc.next();
+                            tareaDescrip = sc.nextLine();
 
                             gest.insertarTarea(tareaNombre, tareaDescrip);
                             
@@ -332,16 +346,18 @@ public class PIProgEnt {
                             
                             System.out.println("Escriba el ID de la tarea que quiere modificar: ");
                             tareaID = sc.nextInt();
+                            sc.nextLine();
                             System.out.println("Escriba el nombre de la tarea: ");
-                            tareaNombre = sc.next();
+                            tareaNombre = sc.nextLine();
                             System.out.println("Escriba una descripción breve: ");
-                            tareaDescrip = sc.next();
+                            tareaDescrip = sc.nextLine();
                             
                             opt=0;
                             break;
                         case 3:
                             System.out.println("Escriba el ID de la tarea que busca: ");
                             int idTarea = sc.nextInt();
+                            sc.nextLine();
                             gest.buscarTarea(idTarea);
                             opt=0;
                             break;
@@ -352,6 +368,7 @@ public class PIProgEnt {
                         case 5:
                             System.out.println("Escriba el ID de la tarea que quiere eliminar: ");
                             tareaID = sc.nextInt();
+                            sc.nextLine();
                             gest.eliminarTarea(tareaID);
                             opt=0;
                             break;
